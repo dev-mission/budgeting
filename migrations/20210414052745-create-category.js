31b0fe62721b66d0694166f062a9942356a54e80
chucklui <77710805+chucklui@users.noreply.github.com>
2021-04-14 19:42:09 -0700

    Added a Category datamodel (#10)

---
+++ migrations/20210414052745-create-category.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      UserId: {
+        type: Sequelize.INTEGER
+      },
+      categoryName: {
+        type: Sequelize.STRING
+      },
+      limit: {
+        type: Sequelize.DECIMAL
+      },
+      spend: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Categories');
+  }
+};
