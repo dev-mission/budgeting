6c416d2629ba500c33ed6fdf90d961604b5a5476
disheng-wang <seanwangucm@gmail.com>
2021-04-28 21:34:38 -0700

    Revert "Sean user api setup (#16)" (#18)
    This reverts commit d2ad2cdf087d6d9b0959ce408d85024d5c82c6ad.

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,6 +6,5 @@ router.use('/auth', require('./auth'));
 router.use('/passwords', require('./passwords'));
 router.use('/uploads', require('./uploads'));
 router.use('/users', require('./users'));
-Router.use('/budget', require("./budget");
 
 module.exports = router;
