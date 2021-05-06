9b02f5b6a18b72d67f27ef1c4fe2ff01d41f4973
Demere Smith <demeresmith5@gmail.com>
2021-05-05 20:25:52 -0700

    Demere resource and faq page

---
+++ client/src/Faq/Faq.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Faq = () => {
+    return (
+        <div> 
+            <h1>Get all of your questions about Bulga answered hear!</h1>
+
+            <h4>Will my budget be saved when I log out?</h4>
+            <p>Yes, your budget is saved automatically.</p>
+
+            <h4>Do I have to pay to use Bulga?</h4>
+            <p>No, Bulga is completely free to use. We just want to help you save!</p>
+            
+            <h4>Will you sell my information?</h4>
+            <p>No, your information will be kept private.</p>
+            
+            <h4>Can I choose the color I want for my expenses?</h4>
+            <p>Yes, you do this during the "Adding Expenses" category in step 3 of igning up.  </p>
+           
+            <h4>Can I keep track of my savings weekly or yearly?</h4>
+            <p>As of now you only can keep track of your savings monthly.</p>
+        </div> 
+
+    )
+}
+
+export default Faq; 
