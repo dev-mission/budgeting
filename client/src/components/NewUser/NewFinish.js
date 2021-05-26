58012ed83e8ffef1c30d0df133287d924b3cd8e0
chucklui <77710805+chucklui@users.noreply.github.com>
2021-05-25 23:25:59 -0700

    Chuck newuser pagestyle (#30)
    * chuck will continue to do the style for new budget page
    
    * chuck add some styling to the user setup page
    
    * chuck add the new button on the expenseslist file
    
    * chuck add some styling but still need a lot of work
    
    * new user pages styling
    
    * folder lowercase c
    
    * need help on styling more on the user pages
    
    * Added a scss file for NewCategories

--- client/src/components/NewUser/NewFinish.js
+++ client/src/components/NewUser/NewFinish.js
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom';
+import { UserArrow } from '../../shared/logo/Avatars';
+import "../../shared/style/SetUp.css";
 function NewFinish (){
     return (
-        <div>
-            <ul>
-                <li>Step 0 Create Account</li>
-                <li>Step 1 Determine Spending Budget</li>
-                <li>Step 2 SetUp Saving Goals</li>
-                <li>Step 3 Add Expense Categories</li>
-            </ul>
-            <div>
-                <h1>Great, you’re ready to start budgeting and saving with Bulga!</h1>
+        <div className="setup">
+            <div className="steps">
+                <ul>
+                    <li>Step 0 Create Account</li>
+                    <li>Step 1 Determine Spending Budget</li>
+                    <li>Step 2 SetUp Saving Goals</li>
+                    <li>Step 3 Add Expense Categories</li>
+                </ul>
             </div>
-            <Link className="btn btn-primary" to="/overview">ARROW</Link>
+            <div className="background">
+                <h2 className="setupFinish">Great, you’re ready to start budgeting and saving with Bulga!</h2>
+                <Link className="arrow" to="/overview"><UserArrow /></Link>
+            </div>
         </div>
     )
 }
